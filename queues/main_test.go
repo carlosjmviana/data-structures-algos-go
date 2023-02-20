fcaa10dfcc6aa306995fa7129b2b0434ad4db9c6
Carlos Juliano M Viana <carlos.viana@olxbr.com>
2023-02-20 09:44:44 -0300

    Added test for Dequeue function
    Issue: #2

--- queues/main_test.go
+++ queues/main_test.go
@@ -15,3 +15,15 @@ func TestEnqueue(t *testing.T) {
 	expected := []int{10, 1, 20}
 	assert.Equal(t, expected, myQueue.items)
 }
+
+func TestDequeue(t *testing.T) {
+	myQueue := Queue{}
+	myQueue.Enqueue(10)
+	myQueue.Enqueue(1)
+	myQueue.Enqueue(20)
+
+	myQueue.Dequeue()
+
+	expected := []int{1, 20}
+	assert.Equal(t, expected, myQueue.items)
+}
