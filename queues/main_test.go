3a591cf032e43e64bc94c0cb83dff89a29e05d16
Carlos Juliano M Viana <carlos.viana@olxbr.com>
2023-02-20 09:42:23 -0300

    Added data structures for queues and stack.
    Issue: #2

---
+++ queues/main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEnqueue(t *testing.T) {
+	myQueue := Queue{}
+	myQueue.Enqueue(10)
+	myQueue.Enqueue(1)
+	myQueue.Enqueue(20)
+
+	expected := []int{10, 1, 20}
+	assert.Equal(t, expected, myQueue.items)
+}
